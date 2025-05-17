044767ac76904caa01bfae1b1a1bc063c5c07165
HaithamZedan1 <hathnzdon@gmail.com>
2025-05-17 15:34:21 +0300

    new urls added

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('items/', views.MenuView.as_view()),
+    path('items/<int:pk>', views.SingleMenuView.as_view()),
 ]
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+from .models import Menu,Booking
+from .serializers import BookingSerializer,MenuSerializer
+#from django.contrib.auth import permissions
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class BookingViewSet(ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    #permission_classes = [permissions.IsAuthenticated] 
+
